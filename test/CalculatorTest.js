44e0c61ff917b91eb15d65547ca9d613bc89578f
Luigi Lake <lfdlake@gmail.com>
2018-01-10 23:47:46 -0500

    fix run commands, babel dependencies, mocha to ES6

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -1,8 +1,7 @@
-var expect = require('chai').expect;
-var calcFlush = require('../public/flush')
-// import 'babel-polyfill'
-// import { expect } from 'chai';
-// import calcFlush from '../public/flush';
+// var expect = require('chai').expect;
+// var calcFlush = require('../public/flush')
+import { expect } from 'chai';
+import calcFlush from '../public/flush';
 
 let data = { 'HEARTS': 5 }
 
@@ -10,6 +9,6 @@ it('should pass', () => {
   expect(true).to.equal(true);
 });
 
-// it('should return true', () => {
-//   expect(calcFlush(data)).to.equal(true);
-// })
+it('should return true', () => {
+  expect(calcFlush(data)).to.equal(true);
+})
