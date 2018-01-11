d0e05522ce18ce0ddeb42220d73309791509482d
Luigi Lake <lfdlake@gmail.com>
2018-01-10 23:30:45 -0500

    add babel to mocha

--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -1,5 +1,15 @@
 var expect = require('chai').expect;
+var calcFlush = require('../public/flush')
+// import 'babel-polyfill'
+// import { expect } from 'chai';
+// import calcFlush from '../public/flush';
+
+let data = { 'HEARTS': 5 }
 
 it('should pass', () => {
-  expect(true).to.equal(true)
-})
+  expect(true).to.equal(true);
+});
+
+// it('should return true', () => {
+//   expect(calcFlush(data)).to.equal(true);
+// })
